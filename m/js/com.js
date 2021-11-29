030c272d53e144c54b8ebadf32445380e673261d
김성은 <kse012050@naver.com>
2021-11-29 16:04:18 +0900

    mobile 원룸페이지 완료

--- m/js/com.js
+++ m/js/com.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-    $('[data-click]').click(function(){
+    /* $('[data-click]').click(function(){
         var selector = $('.' + $(this).attr('data-click'));
         selector.addClass('active');
         selector.find('.close').click(function(){
             selector.removeClass('active');
         });
+    }); */
+
+    $('[data-click="gnbSearchBox"]').click(function(){
+        var selector = $('.gnbSearchBox');
+        selector.addClass('active');
+        selector.find('.close').click(function(){
+            selector.removeClass('active');
+        });
     });
+
+    $('[data-click="next"]').click(function(){
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    })
 })
